b927342d0cb62fe08deeb73c2ba98c74608fb611
GerardoArdon <20235947@esen.edu.sv>
2025-04-03 16:14:18 -0600

    Correccion en appointment

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -19,37 +19,31 @@ class Appointment extends Model
         'status'
     ];
 
-    // Relación con clientes
     public function customer()
     {
         return $this->belongsTo(Customer::class);
     }
 
-    // Relación con empleados
     public function employee()
     {
         return $this->belongsTo(Employee::class);
     }
 
-    // Relación con servicios
     public function service()
     {
         return $this->belongsTo(Service::class);
     }
 
-    // Accesor para obtener el nombre del cliente en lugar del ID
     public function getCustomerNameAttribute()
     {
         return $this->customer ? $this->customer->name : 'No asignado';
     }
 
-    // Accesor para obtener el nombre del empleado en lugar del ID
     public function getEmployeeNameAttribute()
     {
         return $this->employee ? $this->employee->name : 'No asignado';
     }
 
-    // Accesor para obtener el nombre del servicio en lugar del ID
     public function getServiceNameAttribute()
     {
         return $this->service ? $this->service->name : 'No asignado';
